c18e53a774433ed75d3130c068f9abf457e3aa7c
Sergey <92508690+SN737@users.noreply.github.com>
2022-08-25 17:07:26 +0300

    update

--- task_3/script.js
+++ task_3/script.js
@@ -1,5 +1,12 @@
-const btn = document.querySelector('.btn');
+const url = 'wss://echo-ws-service.herokuapp.com';
+const btnSend = document.querySelector('.btn--send');
+const btnGeo = document.querySelector('.btn--geo');
+const messages = document.querySelector('.messagearea');
+let websocket;
 
-btn.addEventListener('click', ()=> {
-    
-});
+btnSend.addEventListener('click', sendMsg);
+
+
+function sendMsg(){
+   websocket = new websocket(url); 
+}
