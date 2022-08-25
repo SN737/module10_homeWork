82584a4ffefa14dccd8644f487e5c9c37008bf80
Sergey <92508690+SN737@users.noreply.github.com>
2022-08-25 18:39:33 +0300

    Update script.js

--- task_3/script.js
+++ task_3/script.js
@@ -5,8 +5,28 @@ const messages = document.querySelector('.messagearea');
 let websocket;
 
 btnSend.addEventListener('click', sendMsg);
+function createMessage(){
+    const outMsg =
+
+}
 
 
 function sendMsg(){
    websocket = new websocket(url); 
-}
+   websocket.onopen = function(evt) {
+    writeToScreen("CONNECTED");
+  };
+  websocket.onclose = function(evt) {
+    writeToScreen("DISCONNECTED");
+  };
+  websocket.onmessage = function(evt) {
+    writeToScreen(
+      '<span style="color: blue;">RESPONSE: ' + evt.data+'</span>'
+    );
+  };
+  websocket.onerror = function(evt) {
+    writeToScreen(
+      '<span style="color: red;">ERROR:</span> ' + evt.data
+    );
+  };
+};
