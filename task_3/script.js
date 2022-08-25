40e9340a891981d5ceac62c6697745e282e40d54
Sergey <92508690+SN737@users.noreply.github.com>
2022-08-25 22:23:57 +0300

    update

--- task_3/script.js
+++ task_3/script.js
@@ -1,32 +1,81 @@
 const url = 'wss://echo-ws-service.herokuapp.com';
 const btnSend = document.querySelector('.btn--send');
 const btnGeo = document.querySelector('.btn--geo');
+const input = document.querySelector('.inputfield');
 const messages = document.querySelector('.messagearea');
 let websocket;
+document.addEventListener('DOMContentLoaded', riseConnection);
 
-btnSend.addEventListener('click', sendMsg);
-function createMessage(){
-    const outMsg =
+input.addEventListener('input', () => {
+  if(!input.value){
+      btnSend.disabled = true;
+  } else{
+      btnSend.disabled = false;}  
+});
+
+
+btnSend.addEventListener('click', ()=>{
+  if (!input.value) {
+    
+    return;
+    }else { 
+      createMessage(input.value);}
+    });
+
+
+function createMessage(textMsg){
+    const outMsg =document.createElement('div');
+    outMsg.textContent = 'отправленная мессага' + textMsg;
+    outMsg.classList.add('outMsg');
+    messages.append(outMsg);
+    sendMsg(textMsg);
+
+}
+
+function recievMessage(){
+  const inMsg =document.createElement('div');
+    inMsg.textContent = 'полученная мессага';
+    inMsg.classList.add('inMsg');
+    messages.append(inMsg);
 
 }
 
 
-function sendMsg(){
-   websocket = new websocket(url); 
-   websocket.onopen = function(evt) {
-    writeToScreen("CONNECTED");
+  function riseConnection(){
+  websocket = new WebSocket(url); 
+  websocket.onopen = ()=>{ 
+    console.log("CONNECTED");
+    input.disabled = false;
   };
-  websocket.onclose = function(evt) {
-    writeToScreen("DISCONNECTED");
-  };
-  websocket.onmessage = function(evt) {
-    writeToScreen(
-      '<span style="color: blue;">RESPONSE: ' + evt.data+'</span>'
-    );
-  };
-  websocket.onerror = function(evt) {
-    writeToScreen(
-      '<span style="color: red;">ERROR:</span> ' + evt.data
-    );
-  };
+    websocket.onmessage = (e) => {
+      recievMessage(e.data);
+    }; 
+
+    websocket.onclose = (e)=> {
+      console.log("DISCONNECTED");
+      input.disabled = true;
+      btnSend.disabled = true;
+
+
+    };
+        
 };
+  websocket.onclose = function(evt) {
+    console.log("DISCONNECTED");
+  };
+  // websocket.onmessage = (e) => {
+  //   recievMessage(e.data);
+  // }; 
+    
+//   websocket.onerror = function(evt) {
+//     console.log(
+//       '<span style="color: red;">ERROR:</span> ' + evt.data
+//     );
+//   };
+// };
+
+
+async function sendMsg(textMsg){
+  await websocket.send(textMsg);
+  input.value = '';
+}
